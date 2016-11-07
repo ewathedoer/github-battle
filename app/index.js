2afad5aaebf45b0e13d08a91d71d3836d9d51c5c
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-11-07 14:05:11 +0100

    render avatar stage 1

--- app/index.js
+++ app/index.js
@@ -1,15 +1,55 @@
+var USER_DATA = {
+  name: 'Marta Mrowecka',
+  username: 'ant',
+  image: 'http://theonewhodo.es/images/ant.png'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function() {
     return(
-      <div>Hello once again</div>
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    )
+  }
+});
+
+var ProfilLink = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}> 
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return(
+      <div> 
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfilLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />, 
+  <Avatar user={USER_DATA} />, 
   document.getElementById('app')
 );
