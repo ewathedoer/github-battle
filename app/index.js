6d33af904d79eeba368853303145d51614bd15d0
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-11-07 13:10:25 +0100

    set up webpack
    the ground for the project

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return(
+      <div>Hello once again</div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld />, 
+  document.getElementById('app')
+);
