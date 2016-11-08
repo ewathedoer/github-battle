061d1d09baa1e7b5d0a8b0c1ecd61b5f4288d135
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-11-08 10:20:12 +0100

    set up routing

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return(
+      <div>We are at home</div>
+    )
+  }
+});
+
+module.exports = Home;
--- app/index.js
+++ app/index.js
@@ -1,55 +1,8 @@
-var USER_DATA = {
-  name: 'Marta Mrowecka',
-  username: 'ant',
-  image: 'http://theonewhodo.es/images/ant.png'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function() {
-    return(
-      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-    )
-  }
-});
-
-var ProfilLink = React.createClass({
-  render: function() {
-    return(
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}> 
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>
-        {this.props.name}
-      </div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function(){
-    return(
-      <div> 
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfilLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
+var routes = require('./config/routes');
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />, 
+  routes, 
   document.getElementById('app')
 );
