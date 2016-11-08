fc95931d07cbd20ac22c4196872143fe7a129bf4
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-11-08 15:10:02 +0100

    display raw data about each user

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,13 @@
 var React = require('react');
 
+function puke (object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 function ConfirmBattle(props) {
   return props.isLoading === true
   ? <p>Loading!</p>
-  : <p>Confirm Battle!</p>
+  : <div>Confirm Battle!: {puke(props)}</div>
 }
 
 module.exports = ConfirmBattle;
