70d2bd9f9e28c7a7f155ad4b6665185cb9aed3a9
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-11-09 13:02:28 +0100

    display two players for the battle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,4 +1,8 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
 
 function puke (object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -7,7 +11,44 @@ function puke (object) {
 function ConfirmBattle(props) {
   return props.isLoading === true
   ? <p>Loading!</p>
-  : <div>Confirm Battle!: {puke(props)}</div>
+  : <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+      <h1>Confirm Players</h1>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <div className='col-sm-6'>
+          <p className='lead'>
+            Player 1
+          </p>
+          <UserDetails info={(props.playersInfo[0])} />
+        </div>
+        <div className='col-sm-6'>
+          <p className='lead'>
+            Player 2
+          </p>
+          <UserDetails info={(props.playersInfo[1])} />
+        </div>
+      </div>
+          
+      <div className='col-sm-8 col-sm-offset-2'>
+        <div className='col-sm-12' style={styles.space}>
+          <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+            Initatiate Battle
+          </button>
+        </div>
+        <div className='col-sm-12' style={styles.space}>
+          <Link to='/playerOne'>
+            <button type='button' className='btn btn-lg btn-warning'>
+              Reselect Players
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -9,7 +9,7 @@ var ConfirmBattleContainer = React.createClass({
   getInitialState: function() {
     return {
       isLoading: true,
-      playerInfo: []
+      playersInfo: []
     }
   },
   componentDidMount: function() {
@@ -21,10 +21,19 @@ var ConfirmBattleContainer = React.createClass({
       })
     }.bind(this))
   },
+  handleInitiateBattle: function() {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
   render: function() {
     return(
       <ConfirmBattle 
         isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo} />
     )
   }
