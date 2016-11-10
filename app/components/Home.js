172bfb2ad2ef2cf8ed194e1dbc96311e36cc1f84
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-11-10 13:54:39 +0100

    center homepage vertically

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,20 +3,24 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var MainContainer = require('./MainContainer');
 var Img = require('./Img');
+var styles = require('../styles');
+var HomeContainerWrapper = require('./HomeContainerWrapper');
 
 var Home = React.createClass({
   render: function() {
     return(
-      <MainContainer>
-        <h1>Github Battle</h1>
-        <p className="lead">Compare Developers</p>
-        <Link to='/playerOne'>
-          <button type='button' className='btn btn-lg cta'>
-            Get Started
-          </button>
-        </Link>
-        <Img />
-      </MainContainer>
+      
+        <HomeContainerWrapper>
+          <h1>Github Battle</h1>
+            <p className="lead">Compare Developers</p>
+            <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg cta'>
+                Get Started
+              </button>
+            </Link>
+            <Img />
+        </HomeContainerWrapper>
+      
     )
   }
 });
---
+++ app/components/HomeContainerWrapper.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var styles = require('../styles');
+
+function HomeContainerWrapper (props) {
+  return (
+    <div className="flex-parent">
+      <div className="flex-child">
+        <div className="row">
+          <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+module.exports = HomeContainerWrapper;
